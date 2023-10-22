5135691410a84eba97100d1e23f7365714bbead7
MickKav <mkavanagh187@gmail.com>
2023-10-22 14:31:19 +0000

    Add post model and view

--- news/views.py
+++ news/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic, View
+from django.http import HttpResponseRedirect
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
