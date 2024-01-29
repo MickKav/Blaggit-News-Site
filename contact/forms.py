5281d96863e7099f4f9a53e593215b292067228d
MickKav <mkavanagh187@gmail.com>
2024-01-29 22:14:44 +0000

    Add contact views and form

---
+++ contact/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from phonenumber_field.formfields import PhoneNumberField
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+
+    phone = PhoneNumberField(widget=forms.TextInput(
+        attrs={'placeholder': ('+353123456789')}))
+
+    class Meta:
+        model = Contact
+        fields = (
+            'name',
+            'phone',
+            'email',
+            'message')
---
+++ contact/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import ContactForm
+
+
+def get_user_instance(request):
+    """
+    retrieves user details if logged in
+    """
+
+    user_email = request.user.email
+    user = User.objects.filter(email=user_email).first()
+    return user
+
+
+class ContactMessage(View):
+    """
+    This view displays the contact form and if the user
+    is registered and inserts the user email into the
+    email field
+    """
+    template_name = 'contact/contact.html'
+    success_message = 'Message has been sent.'
+
+    def get(self, request, *args, **kwargs):
+        """
+        Retrieves users email and inputs into email input
+        """
+        if request.user.is_authenticated:
+            email = request.user.email
+            contact_form = ContactForm(initial={'email': email})
+        else:
+            contact_form = ContactForm()
+        return render(request, 'contact/contact.html',
+                      {'contact_form': contact_form})
+
+    def post(self, request):
+        """
+        Checks that the provided info is valid format
+        and then posts to database
+        """
+        contact_form = ContactForm(data=request.POST)
+
+        if contact_form.is_valid():
+            contact = contact_form.save(commit=False)
+            contact.user = request.user
+            contact.save()
+            messages.success(
+                request, "Message has been sent")
+            return render(request, 'contact/received.html')
+
+        return render(request, 'contact/contact.html',
+                      {'contact_form': contact_form})
