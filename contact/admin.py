10e188a160aa0af57b103b832e553d02d26da96d
MickKav <mkavanagh187@gmail.com>
2024-01-29 22:19:22 +0000

    Add admin details and wire apps.py

---
+++ contact/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from rangefilter.filters import DateRangeFilter
+from .models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_filter = (
+        'user',
+        'name',
+        'email',
+        'phone',
+        'created_date'
+        )
+    list_display = (
+        'message_id',
+        'user',
+        'name',
+        'message',
+        'phone',
+        'created_date')
+
+    search_fields = ['name']
+    list_filter = (('created_date', DateRangeFilter),)
