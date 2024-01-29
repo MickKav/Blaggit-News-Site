75a958ff42ccdd82930c9e0568accfaad52933e3
MickKav <mkavanagh187@gmail.com>
2024-01-29 22:23:13 +0000

    Import views to urls in contact app

--- contact/urls.py
+++ contact/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('contact/', views.ContactMessage.as_view(), name='contact'),
