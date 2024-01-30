22738e8696b18df9dd7cdfbb4b53dfe9f5154d85
MickKav <mkavanagh187@gmail.com>
2024-01-30 18:12:48 +0000

    Add placeholders to contact form via models

--- contact/forms.py
+++ contact/forms.py
@@ -9,6 +9,10 @@ class ContactForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper(self)
+        self.fields['name'].widget.attrs['placeholder'] = 'Your Name'
+        self.fields['phone'].widget.attrs['placeholder'] = '+353123456789'
+        self.fields['email'].widget.attrs['placeholder'] = 'Your Email'
+        self.fields['message'].widget.attrs['placeholder'] = 'Your Message'
 
     phone = PhoneNumberField(widget=forms.TextInput(
         attrs={'placeholder': ('+353123456789')}))
